9fed94edc7a6819489849b633e926804bc6a2cfc
Changhyun Kwon <chkwon@gmail.com>
2018-01-24 08:14:11 -0500

    ipopt test

--- Formula/ipopt.rb
+++ Formula/ipopt.rb
@@ -1,62 +1,40 @@
 class Ipopt < Formula
-  desc "Large-scale nonlinear optimization package"
-  homepage "https://projects.coin-or.org/Ipopt"
-  url "https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.8.tgz"
-  sha256 "62c6de314220851b8f4d6898b9ae8cf0a8f1e96b68429be1161f8550bb7ddb03"
+  homepage 'https://projects.coin-or.org/Ipopt'
+  url 'http://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.4.tgz'
+  sha256 '292afd952c25ec9fe6225041683dcbd3cb76e15a128764671927dbaf881c2e89'
   revision 1
-  head "https://projects.coin-or.org/svn/Ipopt/trunk", :using => :svn
 
-  bottle :disable, "needs to be rebuilt with latest open-mpi"
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    rebuild 2
+    sha256 "212196e3838345ed1e16c962514ddaf32c2e5a822f797f2af9b05cb130ec3d7a" => :sierra
+    sha256 "2d240f0c450c65ea0555844e117d8f10a1f565f8b732538dc023fe50404d7d71" => :el_capitan
+    sha256 "33c266d3fb148cb96a381baeee7d5edd368891cda1bd935da1e9f98c235efced" => :yosemite
+ end
 
-  option "without-test", "Skip build-time tests (not recommended)"
-  deprecated_option "without-check" => "without-test"
-
-  depends_on "ampl-mp" => :recommended
-  depends_on "openblas" => :optional
-  depends_on "pkg-config" => :build
-
-  # IPOPT is not able to use parallel MUMPS.
-  depends_on "mumps" => ["without-mpi"] + ((build.with? "openblas") ? ["with-openblas"] : [])
-
-  depends_on "gcc"
-
-  def install
-    ENV.delete("MPICC")  # configure will pick these up and use them to link
-    ENV.delete("MPIFC")  # which leads to the linker crashing.
-    ENV.delete("MPICXX")
-    mumps_libs = %w[-ldmumps -lmumps_common -lpord -lmpiseq]
-    mumps_incdir = Formula["mumps"].opt_libexec/"include"
-    mumps_libcmd = "-L#{Formula["mumps"].opt_lib} " + mumps_libs.join(" ")
-
-    args = ["--disable-debug",
-            "--disable-dependency-tracking",
-            "--prefix=#{prefix}",
-            "--with-mumps-incdir=#{mumps_incdir}",
-            "--with-mumps-lib=#{mumps_libcmd}",
-            "--enable-shared",
-            "--enable-static"]
-
-    if build.with? "openblas"
-      args << "--with-blas-incdir=#{Formula["openblas"].opt_include}"
-      args << "--with-blas-lib=-L#{Formula["openblas"].opt_lib} -lopenblas"
-      args << "--with-lapack-incdir=#{Formula["openblas"].opt_include}"
-      args << "--with-lapack-lib=-L#{Formula["openblas"].opt_lib} -lopenblas"
-    end
-
-    if build.with? "ampl-mp"
-      args << "--with-asl-incdir=#{Formula["ampl-mp"].opt_include}/asl"
-      args << "--with-asl-lib=-L#{Formula["ampl-mp"].opt_lib} -lasl"
-    end
-
-    system "./configure", *args
-    system "make"
-    ENV.deparallelize # Needs a serialized install
-    system "make", "test" if build.with? "test"
-    system "make", "install"
+  # Need this snippet in every formula that has a runtime dependency on libgfortran
+  def post_install
+    fixup_libgfortran prefix
   end
 
-  test do
-    # IPOPT still fails to converge on the Waechter-Biegler problem?!?!
-    system "#{bin}/ipopt", "#{Formula["ampl-mp"].opt_pkgshare}/example/wb" if build.with? "ampl-mp"
+  # Need to enable this when building the bottle, disable it when installing from bottles
+  depends_on "gcc" if ARGV.build_bottle?
+
+  def install
+    system "cd ThirdParty/Blas; ./get.Blas"
+    system "cd ThirdParty/Blas; ./configure --prefix=#{prefix} --disable-shared --with-pic; make install"
+    system "cd ThirdParty/Lapack; ./get.Lapack"
+    system "cd ThirdParty/Lapack; ./configure --prefix=#{prefix} --disable-shared --with-pic; make install"
+    system "cd ThirdParty/ASL; ./get.ASL"
+    system "cd ThirdParty/Mumps; ./get.Mumps"
+    system "./configure", "--disable-dependency-tracking",
+                          "--with-blas=#{prefix}/lib/libcoinblas.a",
+                          "--with-lapack=#{prefix}/lib/libcoinlapack.a",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make test"
+    ENV.deparallelize  # make install fails in parallel.
+    system "make install"
   end
 end
