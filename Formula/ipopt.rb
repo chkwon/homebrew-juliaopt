a11a25e4969de1f27f8de5c471697fb6c885947a
Changhyun Kwon <chkwon@gmail.com>
2018-01-23 22:13:16 -0500

    :fortran --> "gcc"

--- Formula/ipopt.rb
+++ Formula/ipopt.rb
@@ -18,7 +18,7 @@ class Ipopt < Formula
   # IPOPT is not able to use parallel MUMPS.
   depends_on "mumps" => ["without-mpi"] + ((build.with? "openblas") ? ["with-openblas"] : [])
 
-  depends_on :fortran
+  depends_on "gcc"
 
   def install
     ENV.delete("MPICC")  # configure will pick these up and use them to link
