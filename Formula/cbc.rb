a2f251a3294bbe6561082e7b8cd68a5b17ac11f0
Changhyun Kwon <chkwon@gmail.com>
2018-01-24 12:05:10 -0500

    cbc bottle

--- Formula/cbc.rb
+++ Formula/cbc.rb
@@ -3,6 +3,12 @@ class Cbc < Formula
   url 'http://www.coin-or.org/download/source/Cbc/Cbc-2.9.7.tgz'
   sha256 '637d080d381e620888d032cba28a3f1f8199f3b5619a3b763aa1470fda543817'
 
+  bottle do
+    root_url 'https://dl.bintray.com/chkwon/juliaopt'
+    cellar :any
+    sha256 "164ebfa986874a29cef9650c7fd4974213eb28907d364ef8821296bd991e93e0" => :high_sierra
+  end
+
   def install
     # build without lapack until Julia issue 4923 is resolved
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
